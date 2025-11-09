4ed1a0a2279e4401f1c29d408c6c5266279d6ab7
Kaushik Varadharajan <me@kaushikv.com>
2025-11-09 10:46:28 -0600

    proof with sorry

--- Proofs/Basic.lean
+++ Proofs/Basic.lean
@@ -1,3 +1,45 @@
-import Mathlib
+import Mathlib.Computability.Language
+import Mathlib.Computability.MyhillNerode
+import Mathlib.Data.Set.Finite.Basic
 
-def hello := "world"
+def zero : Bool := false
+def one : Bool := true
+
+def L01 : Language Bool :=
+  { w | ∃ n : ℕ, w = List.replicate n zero ++ List.replicate n one }
+
+theorem L01_is_irregular : ¬L01.IsRegular := by
+
+  have h : (Set.range L01.leftQuotient).Infinite := by {
+
+    -- "fooling set"
+    let f := fun n : ℕ => L01.leftQuotient (List.replicate n zero)
+
+    let f_is_injective : Function.Injective f := by {
+      intro (i : ℕ) (j : ℕ)
+      contrapose
+      intro i_ne_j
+
+      let x := List.replicate i zero
+      let y := List.replicate j zero
+      let z := List.replicate i one -- distinguishing suffix
+
+      have xz_in_L : (x ++ z ∈ L01) := by use i
+      have yz_not_in_L : (y ++ z ∉ L01) := by {
+        by_contra h
+
+        sorry
+      }
+      exact ne_of_mem_of_not_mem' xz_in_L yz_not_in_L
+    }
+
+    let f_within_s : (∀ (x : ℕ), f x ∈ (Set.range L01.leftQuotient)) := by {
+      exact fun x ↦ Set.mem_range_self (List.replicate x zero)
+    }
+
+    exact Set.infinite_of_injective_forall_mem f_is_injective f_within_s
+  }
+
+  by_contra l_is_regular
+  let set_is_finite := Language.IsRegular.finite_range_leftQuotient l_is_regular
+  contradiction
